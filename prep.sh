c4591db0bbec5190a0f55f85af5d6676a8304a24
Kaiwalya Kher <knkher@gmail.com>
2013-03-25 19:41:15 -0700

    Adding "make prep" to initialize environment.

---
+++ prep.sh
@@ -0,0 +1,18 @@
+if [ ! -e ~/.nvm/install.sh ];
+then
+	echo "Installing NVM..."
+	curl https://raw.github.com/creationix/nvm/master/install.sh | sh $LOG
+else
+	echo "Updating NVM..."
+	~/.nvm/install.sh
+fi
+. ~/.profile
+nvm install 0.10.1
+nvm use 0.10.1
+npm install -g jake
+npm install -g bunyan
+npm install -g jshint
+npm install -g node-inspector
+npm install -g node-dev
+npm install -g supervisor
+npm install -g mocha
