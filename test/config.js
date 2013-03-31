1c588607d18d393401266ac3ae5e445bf8a27058
Kaiwalya Kher <knkher@gmail.com>
2013-03-31 06:31:47 -0700

    Refactoring configuration settings.

--- test/config.js
+++ test/config.js
@@ -3,12 +3,31 @@
 var conf = require('../lib/config');
 
 
-var config = new conf.Configuration();
-
-describe('class Configuration', function () {
-	describe('getAPIHost()', function () {
-		it('Returns the API Host', function (done) {
-			config.getAPIHost();
+describe('module config', function () {
+	it('Manages configurations for the website, webapi and the authorization. It also makes ', function (done) {
+		done();
+	});
+	describe('function loadConfig(who)', function () {
+		it('Constructs a new configuration for use by "who". "who" can be "site" or "api"', function (done) {
+			var configSite = new conf.loadConfig('site');
+			var configAPI = new conf.loadConfig('api');
+			var ensureType = function (objName, member, obj, typeExpected) {
+				var typeGot = typeof obj[member];
+				if (typeGot !== typeExpected) {
+					throw new Error("typeof (" + objName + "." + member + ") is [" + typeGot + "] expected [" + typeExpected + "]");
+				}
+			};
+			var checkConfig = function (configName, config) {
+				ensureType(configName, "secure", config, 'boolean');
+				ensureType(configName, "hostName", config, 'string');
+				ensureType(configName, "port", config, 'number');
+				if (config.secure) {
+					ensureType(configName, "certificate", config, 'string');
+					ensureType(configName, "key", config, 'string');
+				}
+			};
+			checkConfig('configAPI', configAPI);
+			checkConfig('configSite', configSite);
 			done();
 		});
 	});
--- test/test.js
+++ test/test.js
@@ -3,38 +3,17 @@ var packageRoot = '../';
 var service = require(packageRoot + 'lib/service');
 
 describe('service', function () {
-	it('Exports ServiceConfig', function (done) {
-		if (!service.ServiceConfig) {
-			throw "Server is not exporting server config.";
+	it('Exports config', function (done) {
+		if (!service.config) {
+			throw new Error("Server is not exporting server config.");
 		}
 		done();
-	});
-	describe('ServiceConfig()', function () {
-		it('Creates a class which contains the host and the port (sc.host, sc.post) on which the server was created', function (done) {
-			var sc = new service.ServiceConfig();
-			if (!sc || !sc.getAPIHost() || !sc.getAPIPort()) {
-				throw "ServiceConfig should construct an object in which Host and port should be defined.";
-			}
-			done();
-		});
-		describe('getAPIEndPoint()', function () {
-			it('Returns a string containing the API Endpoint', function (done) {
-				var sc = new service.ServiceConfig();
-				var str = sc.getAPIEndPoint();
-				if (typeof str === "string") {
-					done();
-				}
-				else {
-					throw "Did not return string";
-				}
-			});
-		});
 	});
 	describe('Http API', function () {
 		describe('/version', function () {
 			it('GET', function (done) {
 				var request = require('request');
-				var ep = new service.ServiceConfig().getAPIEndPoint();
+				var ep = service.config.getEndpoint();
 				var opt = {
 					url: ep + "/version",
 					method: 'GET'
